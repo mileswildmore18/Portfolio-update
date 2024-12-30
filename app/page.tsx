3e4097123f029d36a71fc800e95c16fcbbb48a96
Miles Wildmore <mileswildmore18@gmail.com>
2024-12-30 11:30:31 -0500

    Started on the page section with the layout properties and added the Hero tsx file to start on the top of the website.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-   <main>
-    <div>
-      <h1>Hello, Portfolio</h1>
-    </div>
-   </main>
+    // Display the color of the background for the website and responsive on different size screens
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
+      </div>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,15 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  // Provide the default color for the website
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+     colors: {
+      black: {
+        DEFAULT: '#000',
+        100: '#000319'
+      }
+     }
     },
   },
   plugins: [],

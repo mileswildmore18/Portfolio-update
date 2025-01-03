3a556f715cfcd8405aadc3e8ed7f1b8f975cb0c9
Miles Wildmore <mileswildmore18@gmail.com>
2025-01-03 15:50:21 -0500

    Added svg files to the public folder as well as cleaning up the files to make it easier to navigate.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import Hero from "@/components/Hero";
-import { FloatingNavbar } from"@/components/ui/FloatingNavbar";
-import Grid from "@/components/ui/Grid";
+import { FloatingNavbar } from "@/components/ui/FloatingNavbar";
+import Grid from "@/components/Grid";
 import { FaHome } from "react-icons/fa";
 export default function Home() {
   return (
     // Display the color of the background for the website and responsive on different size screens
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNavbar navItems={[
-          {name: 'Home', link: '/', icon: <FaHome /> }
-        ]}/> 
+        <FloatingNavbar
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
         <Hero />
         <Grid />
       </div>
---
+++ components/Grid.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
+
+const Grid = () => {
+  return (
+    // Add the Bento Grid and the items
+    <section id="about">
+      <BentoGrid>
+        {/* Display the grid */}
+        {gridItems.map((item) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
